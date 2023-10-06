7326968a9db8916cf6b52de02221361a37012c85
Suyatul121 <147089588+Suyatul121@users.noreply.github.com>
2023-10-06 09:38:52 +0600

    Rx
    Hlw

---
+++ TEST.py
@@ -0,0 +1,7 @@
+name=str(input("ENTER YOUR NAME:"))#NAME
+number=str(input("ENTER YOUR PHONE NUMBER:"))#PHONE NUMBER
+gmail=str(input("gmail address:"))#USER GMAIL ADDRESS
+
+print("YOUR NAME :"+name)
+print("YOUR NUMBER :"+number)
+print("YOUR GMAIL :"+gmail)
